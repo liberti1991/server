fc8efe1ab97c174dc6d9fdcf13a786687a101824
Liberti1991 <liberti1991@gmail.com>
2022-11-01 14:13:28 -0300

    parando no cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import Fastify from "fastify";
+import { PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function booststrap() {
  const fastify = Fastify({
   logger: true,
  }) 
 
- fastify.get('/pools/count', () => {
-  return { count: 1233 }
+ fastify.get('/pools/count', async () => {
+  const count = await prisma.pool.count()
+
+  return { count }
  })
+
+
 
   await fastify.listen({ port: 3333 })
 
